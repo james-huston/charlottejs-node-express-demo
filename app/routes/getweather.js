7153c780329b3ec82065eb68766ae877455a6715
James Huston <jhuston@redventures.com>
2014-02-19 22:14:36 -0500

    added in lookup of multiple zips at once with tests for 2 and 5

--- app/routes/getweather.js
+++ app/routes/getweather.js
@@ -9,20 +9,74 @@ module.exports = function (req, res) {
     return res.json({ status: 'error'});
   }
 
-  lookupLib.getCurrentWeather(postData.zipcodes, function (err, data) {
-    if (err) {
-      console.log(err);
+  if (postData.zipcodes.length === 5) {
+    console.log('single zip request', postData.zipcodes);
+    lookupLib.getCurrentWeather(postData.zipcodes, function (err, data) {
+      if (err) {
+        console.log(err);
+        return res.json({
+          status: 'error',
+          error: err.toString()
+        });
+      }
+
       return res.json({
-        status: 'error',
-        error: err.toString()
+        status: 'ok',
+        results: 1,
+        data: {
+          currently: data
+        }
+      });
+    });
+  } else if (postData.zipcodes.indexOf(' ') > 0) {
+    console.log('multizip request', postData.zipcodes);
+    // split our string
+    var zipArray = postData.zipcodes.split(' ');
+
+    // remove anything that doesn't look like a zip
+    for (var loopy = 0; loopy < zipArray.length; loopy++) {
+      if ('string' !== typeof zipArray[loopy] || zipArray[loopy].length !== 5) {
+        zipArray.splice(loopy, 1);
+      }
+    }
+
+    // bail if we don't have any zips
+    if (!zipArray.length) {
+      console.log('invalid list of zips');
+      return res.json({
+        status: 'error'
       });
     }
 
-    res.json({
-      status: 'ok',
-      data: {
-        currently: data
-      }
-    });
-  });
+    var results = {};
+    var responses = 0;
+
+    var getZipData = function (zipcode) {
+      console.log('looking up ', zipcode);
+      lookupLib.getCurrentWeather(zipcode, function (err, data) {
+        console.log('got response', zipcode);
+        responses++;
+
+        if (err) {
+          results[zipcode] = {};
+          console.log('error lookup up zipcode', zipcode, err.toString());
+          return;
+        }
+
+        results[zipcode] = {
+          currently: data
+        };
+
+        if (responses === zipArray.length) {
+          return res.json({
+            status: 'ok',
+            results: zipArray.length,
+            data: results
+          });
+        }
+      });
+    }
+
+    zipArray.forEach(getZipData);
+  }
 };
--- test/getweather.test.js
+++ test/getweather.test.js
@@ -26,4 +26,51 @@ describe('Our server getweather route', function () {
         });
     });
   });
+
+  describe('when posting 2 valid zips', function () {
+    it('should provide results for both', function (done) {
+      request
+        .post('http://localhost:' + port + '/getweather')
+        .send({
+          zipcodes: '90210 44093'
+        })
+        .set('Accept', 'application/json')
+        .end(function (res) {
+          var data = res.body || {};
+
+          data.status.should.equal('ok');
+          data.results.should.equal(2);
+
+          data.data['90210'].should.be.an.instanceOf(Object);
+
+          data.data['44093'].should.be.an.instanceOf(Object);
+          done();
+        });
+    });
+  });
+
+  describe('when posting 5 valid zips', function () {
+    it('should provide results for all', function (done) {
+      request
+        .post('http://localhost:' + port + '/getweather')
+        .send({
+          zipcodes: '90210 44093 28173 21431 28210'
+        })
+        .set('Accept', 'application/json')
+        .end(function (res) {
+          var data = res.body || {};
+
+          data.status.should.equal('ok');
+          data.results.should.equal(5);
+
+          data.data['90210'].should.be.an.instanceOf(Object);
+          data.data['44093'].should.be.an.instanceOf(Object);
+          data.data['28173'].should.be.an.instanceOf(Object);
+          data.data['21431'].should.be.an.instanceOf(Object);
+          data.data['28210'].should.be.an.instanceOf(Object);
+
+          done();
+        });
+    });
+  });
 });
